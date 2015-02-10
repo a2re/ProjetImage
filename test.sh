371c3fe0fcf25730ecc6108001d62b0dc7cc55f7
NGUYEN BAO <baonv90@gmail.com>
2015-02-10 23:24:27 +0100

    message

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-filename=$2 
 search_dir=$1
 
 
@@ -19,7 +18,7 @@ done
 ls $search_dir | grep '.ann' | sed 's/.ann//g' > anno_name.txt 
 
 for annofile in `cat anno_name.txt`; do
-	for line in `cat search_dir/$annofile.ann`; do
+	for line in `cat $search_dir/$annofile.ann`; do
 	     val=`expr $i % 2`
 	     
 	     if [ $val == 1 ]
@@ -30,20 +29,4 @@ for annofile in `cat anno_name.txt`; do
 
 	done > temp/$annofile.txt
 done
-
-
-count=0
-for line in `cat $filename`; do
-     echo "$line \n"
-     
-     res=$(./read_image $line)
-     for annofile in `cat anno_name.txt`; do
-	val=$(get_annotation "temp/$annofile.txt" $count)	
-	echo "$val$res">>train/color_$annofile.svm
-	
-     done
-     count=$(($count+1))
-done
-
-rm annotation.txt
 
