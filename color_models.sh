50d31de4a85f52640e79503654240697fe4b352a
toure <toureelhadj@gmail.com>
2015-03-03 06:48:01 +0100

    fix outs

---
+++ color_models.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for line in $(ls train/ann); do
+
+    name=$( echo $line | cut -d"." -f1 )
+    
+    ./libsvm/svm-train -w+1 19 -b 1 train/svm/color_$name.svm train/model/color_$name.model
+
+done
