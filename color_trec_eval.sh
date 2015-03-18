d33ebd03a905c6fbe59defd9ac4a80744bea9c95
toureelhadj <toureelhadj@gmail.com>
2015-03-18 11:18:19 +0100

    Lancer le script en cours

--- color_trec_eval.sh
+++ color_trec_eval.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 for line in $(ls val/rel); do
 
-    name=$( echo $line | cut -d"." -f1 )
+    name=$( echo $line | cut -d "." -f1 )
 
     echo "trec_eval $name: "
 
--- script_hist_sift.sh
+++ script_hist_sift.sh
@@ -17,9 +17,38 @@ for sift in $(ls train/sift/1nn/*.sift); do
 		v[$line]=$((v[$line] + 1))
 	done
 
-	output=$name | cut -d "." -f 1
+	output=$(echo $name | cut -d "." -f 1)
 	for i in `seq 0 255`; do
-		echo $[ v[$i] ] >> "train/hist/$output.svm"
+		if [ $[v[$i]] -ne "0" ]
+		then
+			echo $[ v[$i] ] >> "train/hist/$output.svm"
+		fi
+	done
+done
+
+# Création des val
+rm -R val/hist
+mkdir val/hist
+
+for sift in $(ls val/sift/1nn/*.sift); do
+
+    echo "Traitrement de $sift"
+
+	name=$( basename $sift )
+
+	for i in `seq 0 255`; do
+	    v[$i]=0;
+    done
+
+	for line in $(cat $sift); do
+		v[$line]=$((v[$line] + 1))
 	done
 
+	output=$(echo $name | cut -d "." -f 1)
+	for i in `seq 0 255`; do
+		if [ $[v[$i]] -ne "0" ]
+		then
+			echo $[ v[$i] ] >> "val/hist/$output.svm"
+		fi
+	done
 done
