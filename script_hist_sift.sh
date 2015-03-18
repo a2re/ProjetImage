c6fa1c77a084caecbf7355ba7c8971de77df839a
toureelhadj <toureelhadj@gmail.com>
2015-03-18 17:58:14 +0100

    Suppression des .svm

--- script_hist_sift.sh
+++ script_hist_sift.sh
@@ -1,54 +1,30 @@
 #!/bin/sh
+for f in 'train' 'val'
+do
+	echo "Création du dossier $f"
+	rm -R $f/hist
+	mkdir $f/hist
 
-rm -R train/hist
-mkdir train/hist
+	for sift in $(ls $f/sift/1nn/*.sift); do
 
-for sift in $(ls train/sift/1nn/*.sift); do
+	    echo "Traitrement de $sift"
 
-    echo "Traitrement de $sift"
+		name=$( basename $sift )
 
-	name=$( basename $sift )
+		for i in `seq 0 255`; do
+		    v[$i]=0;
+	    done
 
-	for i in `seq 0 255`; do
-	    v[$i]=0;
-    done
+		for line in $(cat $sift); do
+			v[$line]=$((v[$line] + 1))
+		done
 
-	for line in $(cat $sift); do
-		v[$line]=$((v[$line] + 1))
-	done
-
-	output=$(echo $name | cut -d "." -f 1)
-	for i in `seq 0 255`; do
-		if [ $[v[$i]] -ne "0" ]
-		then
-			echo $[ v[$i] ] >> "train/hist/$output.svm"
-		fi
-	done
-done
-
-# Création des val
-rm -R val/hist
-mkdir val/hist
-
-for sift in $(ls val/sift/1nn/*.sift); do
-
-    echo "Traitrement de $sift"
-
-	name=$( basename $sift )
-
-	for i in `seq 0 255`; do
-	    v[$i]=0;
-    done
-
-	for line in $(cat $sift); do
-		v[$line]=$((v[$line] + 1))
-	done
-
-	output=$(echo $name | cut -d "." -f 1)
-	for i in `seq 0 255`; do
-		if [ $[v[$i]] -ne "0" ]
-		then
-			echo $[ v[$i] ] >> "val/hist/$output.svm"
-		fi
+		output=$(echo $name | cut -d "." -f 1)
+		for i in `seq 0 255`; do
+			if [ $[v[$i]] -ne "0" ]
+			then
+				echo $[ v[$i] ] >> "$f/hist/$output.hist"
+			fi
+		done
 	done
 done
