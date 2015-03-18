ce861ea6ca2aa5720dc4a801b8c64fa0418c6532
Abdourahamane Touré <toureelhadj@gmail.com>
2015-03-18 08:35:01 +0100

    Histogramme sift

---
+++ script_hist_sift.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir train/hist
+
+for sift in $(ls train/1nn/*.sift); do
+
+    echo "Traitrement de $sift"
+
+	name=$( basename $sift )
+
+	for i in 'seq 0 255'; do
+	    v[$i]=0;
+    done
+
+	for line in $(cat $sift); do
+		v[$line]=$([ v[$line] + 1 ])
+	done
+
+	for i in 'seq 0 255'; do
+		echo $[ v[$i] ] >> train/hist/$name
+	done
+
+	break
+done
